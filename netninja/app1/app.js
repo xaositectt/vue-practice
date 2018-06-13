4b23c98ff23804520e8357a7e848d9c62b1a6d3b
xaositectt <tamas.varga.420@gmail.com>
2018-06-13 10:50:13 +0200

    studied filters and did 1-7 netninja tutorials

--- netninja/app1/app.js
+++ netninja/app1/app.js
@@ -5,11 +5,36 @@ const app1 = new Vue({
       return 'hello'
     },
     message2: 'hey',
-    link: 'http://www.google.com'
+    link: 'http://www.google.com',
+    linkString: '<a href="http://www.google.com">click me</a>',
+    age: 23,
+    xcoord: 0,
+    ycoord: 0,
   },
   methods: {
     greet: function(name) {
       return `good day, ${name}, and ${this.message()}`
-    }
+    },
+    add: function(num) {
+      this.age += num
+    },
+    subtract: function(num) {
+      this.age -= num
+    },
+    updateXY: function(event) {
+      console.log(event)
+      this.xcoord = event.offsetX
+      this.ycoord = event.offsetY
+
+    },
+    click: function() {
+      alert('you clicked')
+    },
+    logName: function() {
+      console.log('you entered your name')
+    },
+    logAge: function() {
+      console.log('you entered your age')
+    },
   }
 })
